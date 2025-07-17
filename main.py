473aee4ae6c1a9da491e14756be3d019fe344931
jlo2001 <Jacklowes2001@gmail.com>
2025-07-17 14:46:21 +0100

    WASD player movement

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while 0 < 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = 0.1
+        # dt = Clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
