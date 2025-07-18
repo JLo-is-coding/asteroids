6d1128523935ad2d5db294ff5be6da7ab9b97653
jlo2001 <Jacklowes2001@gmail.com>
2025-07-18 14:38:48 +0100

    added shots and asteroid destroy/split logic

--- main.py
+++ main.py
@@ -4,25 +4,34 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import Shot
 
 def main():
+    # ----- Game Start-up -----
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # ----- Initialisation code -----
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
 
+    # ----- Game Loop -----
     while 0 < 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -33,6 +42,12 @@ def main():
             if collision:
                 print ("Game over!")
                 sys.exit()
+        for object in asteroids:
+            for shot in shots:
+                collision = object.collide(shot)
+                if collision:
+                    object.split()
+                    shot.kill()
         screen.fill("black")
         for item in drawable:
             item.draw(screen)
