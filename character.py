70df0564a7e2608173805e527169b131d9318e4c
jlo2001 <Jacklowes2001@gmail.com>
2025-08-14 19:46:09 +0100

    added ability to change change character font within program

--- character.py
+++ character.py
@@ -1,117 +1,132 @@
 import pygame
+from constants import *
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, x, y, type, colour):
+    def __init__(self, x, y, type, colour, font_width=20, font_height=40):
         super().__init__()
         self.position = pygame.Vector2(x, y)
         self.type = type
         self.colour = colour
+        self.font_width = font_width   
+        self.font_height = font_height
         self.characters = {
 # ===========================================================================
 # A L P H A B E T
 # ===========================================================================    
         "C" :   [(self.position.x, self.position.y), 
-                (self.position.x +20, self.position.y),  
+                (self.position.x +self.font_width, self.position.y),  
                 (self.position.x, self.position.y), 
-                (self.position.x, self.position.y -40),   
-                (self.position.x +20, self.position.y -40)],
+                (self.position.x, self.position.y -self.font_height),   
+                (self.position.x +self.font_width, self.position.y -self.font_height)],
 
         "E" :   [(self.position.x, self.position.y),
-                (self.position.x +20, self.position.y),
+                (self.position.x +self.font_width, self.position.y),
                 (self.position.x, self.position.y),
-                (self.position.x, self.position.y -20),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x, self.position.y -20),
-                (self.position.x, self.position.y -40),
-                (self.position.x +20, self.position.y -40)],    
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -self.font_height)],    
 
         "O" :   [(self.position.x, self.position.y),
-                (self.position.x + 20, self.position.y),
-                (self.position.x + 20, self.position.y - 40),
-                (self.position.x, self.position.y - 40),
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x, self.position.y)],
+
+        "Q" :   [(self.position.x, self.position.y),
+                (self.position.x +(self.font_width/2), self.position.y),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/4)),
+                (self.position.x +((self.font_width/4)*3), self.position.y -(self.font_height/8)),
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width/2, self.position.y -self.font_height/4),
+                (self.position.x +((self.font_width/4)*3), self.position.y -(self.font_height/8)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/4)),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height),
                 (self.position.x, self.position.y)],
 
         "R" :   [(self.position.x, self.position.y),
-                (self.position.x, self.position.y -40),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x, self.position.y -20),
-                (self.position.x + 10, self.position.y -20),
-                (self.position.x +20, self.position.y)],
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x + 10, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y)],
 
         "S" :   [(self.position.x, self.position.y), 
-                (self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x, self.position.y -20),
-                (self.position.x, self.position.y -40),
-                (self.position.x +20, self.position.y -40)],
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -self.font_height)],
 
 # ============================================================================
 # N U M B E R S
 # ============================================================================
-        "1" :   [(self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -40)],
+        "1" :   [(self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height)],
 
         "2" :   [(self.position.x, self.position.y),
-                (self.position.x +20, self.position.y),
+                (self.position.x +self.font_width, self.position.y),
                 (self.position.x, self.position.y),
-                (self.position.x, self.position.y -20),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x, self.position.y -40)],
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height)],
 
         "3" :   [(self.position.x, self.position.y),
-                (self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x, self.position.y -20),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x, self.position.y -40)],
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height)],
 
-        "4" :   [(self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x, self.position.y -20),
-                (self.position.x, self.position.y -40)],
+        "4" :   [(self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -self.font_height)],
 
         "5" :   [(self.position.x, self.position.y), 
-                (self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -20),
-                (self.position.x, self.position.y -20),
-                (self.position.x, self.position.y -40),
-                (self.position.x +20, self.position.y -40)],
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -self.font_height)],
 
-        "6" :  [(self.position.x, self.position.y -20),
-                (self.position.x + 20, self.position.y -20),
-                (self.position.x + 20, self.position.y),
+        "6" :  [(self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y),
                 (self.position.x, self.position.y),
-                (self.position.x, self.position.y -40),
-                (self.position.x +20, self.position.y -40)],
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x +self.font_width, self.position.y -self.font_height)],
         
-        "7" :   [(self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x, self.position.y -40)],
+        "7" :   [(self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height)],
 
-        "8" :   [(self.position.x, self.position.y -20),
+        "8" :   [(self.position.x, self.position.y -(self.font_height/2)),
                 (self.position.x, self.position.y),
-                (self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x, self.position.y -40),
-                (self.position.x, self.position.y -20),
-                (self.position.x +20, self.position.y -20)],
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2))],
 
-        "9" :   [(self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x, self.position.y -40),
-                (self.position.x, self.position.y -20),
-                (self.position.x +20, self.position.y -20)],
+        "9" :   [(self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height),
+                (self.position.x, self.position.y -(self.font_height/2)),
+                (self.position.x +self.font_width, self.position.y -(self.font_height/2))],
                 
         "0" :   [(self.position.x, self.position.y),
-                (self.position.x +20, self.position.y),
-                (self.position.x +20, self.position.y -40),
-                (self.position.x, self.position.y -40),
+                (self.position.x +self.font_width, self.position.y),
+                (self.position.x +self.font_width, self.position.y -self.font_height),
+                (self.position.x, self.position.y -self.font_height),
                 (self.position.x, self.position.y),
-                (self.position.x +20, self.position.y -40)]
+                (self.position.x +self.font_width, self.position.y -self.font_height)]
     }
     
 
